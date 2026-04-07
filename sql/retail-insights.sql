dbbd04b6731340e02d2ebfe3d7d0f1c2434d142c
Ravinandan <ravinandanjn2005@gmail.com>
2026-04-07 23:42:06 +0530

    Insert values into the respective tables

--- sql/retail-insights.sql
+++ sql/retail-insights.sql
@@ -21,3 +21,26 @@ CREATE TABLE sales_transactions (
     quantity_sold INT,
     transaction_date DATE
 );
+
+-- insert categories
+INSERT INTO categories (category_name) VALUES
+('Dairy'),
+('Beverages'),
+('Snacks'),
+('Household');
+
+-- insert products
+INSERT INTO products (product_name, category_id, price, stock_count, expiry_date) VALUES
+('Milk', 1, 50.00, 120, CURRENT_DATE + INTERVAL '5 days'),
+('Cheese', 1, 200.00, 30, CURRENT_DATE + INTERVAL '20 days'),
+('Coke', 2, 45.00, 200, CURRENT_DATE + INTERVAL '60 days'),
+('Chips', 3, 20.00, 10, CURRENT_DATE + INTERVAL '15 days'),
+('Detergent', 4, 150.00, 80, CURRENT_DATE + INTERVAL '365 days');
+
+-- insert sales data
+INSERT INTO sales_transactions (product_id, quantity_sold, transaction_date) VALUES
+(1, 10, CURRENT_DATE - INTERVAL '2 days'),
+(1, 5, CURRENT_DATE - INTERVAL '10 days'),
+(2, 2, CURRENT_DATE - INTERVAL '40 days'),
+(3, 20, CURRENT_DATE - INTERVAL '5 days'),
+(4, 1, CURRENT_DATE - INTERVAL '70 days');
